84bac0f057e4704ae50f914db3e4cc394ed63e33
CaioLeal <lealcaio21@gmail.com>
2026-03-02 19:20:44 -0300

    first commit

---
+++ src/js/home.js
@@ -0,0 +1,15 @@
+export function initHome() {
+    let text = document.querySelector(".text p");
+
+    text.innerHTML = text.innerHTML.split("").map((char, i) => 
+        `<b style="transform:rotate(${i * 6.3}deg)">${char}</b>`
+    ).join("");
+
+    scrollReveal({
+        distance: "90px",
+        duration: 2000,
+        delay: 200
+    });
+
+    scrollReveal().reveal('.hero-info,.main-text,.proposal,.heading');
+}
